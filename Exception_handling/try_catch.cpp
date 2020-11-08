1d64cc4e04fb56808553325bf6037a5431a2dc63
saif-github <saifullabaig785@gmail.com>
2020-11-08 19:12:12 +0530

    Exception handling in c++

---
+++ Exception_handling/try_catch.cpp
@@ -0,0 +1,28 @@
+//C++ program explains flow of exception of try/catch blocks
+
+#include<iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+  int x=-1;
+
+  //Some code
+
+  std::cout << "Before try" << '\n';
+  try{
+    std::cout << "Inside try" << '\n';
+    if(x < 0)
+    {
+      throw x;
+      std::cout << "After trow (never executes)" << '\n';
+    }
+  }
+
+  catch(int x){
+    std::cout << "exception cought" << '\n';
+  }
+  
+  std::cout << "After catch (Will be executed)" << '\n';
+  return 0;
+}
---
+++ oops_concepts/default_constructor.cpp
@@ -0,0 +1,67 @@
+/*
+Consider a class derived from another class with default constructor, or
+a class containing another class object with default onstructor. The compiler needs to insert code to call
+ the default constructor of base class.
+ */
+
+ #include <iostream>
+ using namespace std;
+
+ class Base{
+ public:
+   //Compiler "declares " constructor
+ };
+
+ class A {
+ public:
+   //User defined constructor
+   A()
+   {
+     std::cout << "A constructor" << '\n';
+   }
+   //uninitialized
+   int size;
+ };
+
+ class B : public A{
+   //compiler defines default constructor of B , and
+   //insert stub to call A constructor
+
+   //Compiler won't initialize any data of A
+ };
+
+ class C : public A{
+ public:
+   //compiler defines default constructor of C , and
+   //insert stub to call A constructor
+
+   //Compiler won't initialize any data of A
+   C()
+   {
+     std::cout << "C constructor" << '\n';
+   }
+ };
+
+ class D : public A{
+ public:
+   //compiler defines default constructor of D, and
+   //insert stub to call A constructor
+
+   //Compiler won't initialize any data of A
+   D()
+   {
+     std::cout << "D constructor" << '\n';
+   }
+ private:
+   A a;
+ };
+
+ int main(){
+   Base base;
+
+   B b;
+   C c;
+   D d;
+
+   return 0;
+ }
